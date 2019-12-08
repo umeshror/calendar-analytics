31a2d8362e8645b51341bb704f2326d0f6f5b680
umeshror <sarukumesh@gmail.com>
2019-12-09 01:59:28 +0530

    Expired Access Token handled

--- config/urls.py
+++ config/urls.py
@@ -4,15 +4,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 
-from apps.calendar.api import FetchEventAPIView
 from apps.authenticate.views import OAuth, OAuth2CallBack
+from apps.calendar.views import FetchEventView
 from apps.views import index
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^fetch-events/$', FetchEventAPIView.as_view(), name='fetch_events'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth-login/$', csrf_exempt(OAuth.as_view()), name='oauth'),
+    url(r'^fetch-events/$', FetchEventView.as_view(), name='fetch_events'),
     url(r'^oauth-callback/$', OAuth2CallBack.as_view(), name='oauth2_callback'),
 ]
 if settings.DEBUG:
