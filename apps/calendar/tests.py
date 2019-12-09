1d938c4c37e72b22b90cbb49a02f5e2ef8f9a900
umeshror <sarukumesh@gmail.com>
2019-12-10 01:48:40 +0530

    TestCalendarAnalytics test cases added

--- apps/calendar/tests.py
+++ apps/calendar/tests.py
@@ -1,6 +1,10 @@
+import mock as mock
+import pandas as pd
 from django.contrib.auth.models import User
 from django.test import TestCase
+from pandas._libs.tslibs.timestamps import Timestamp
 
+from apps.calendar.api import CalendarAnalytics
 from apps.calendar.models import Calendar, Event
 from apps.calendar.views import get_or_create_events, get_or_create_calendar, get_organiser, get_utc_time, \
     create_attendees
@@ -167,3 +171,89 @@ class TestEventCreation(TestCase):
         expected_emails = ["admin@admin.com", "admin2@admin.com", "user2@admin.com"]
         self.assertCountEqual(actual_emails, expected_emails)
         self.assertEquals(actual_output[0].event, event)
+
+
+class TestCalendarAnalytics(TestCase):
+    DATA = {'end_time': {0: Timestamp('2019-03-15 13:00:00+0530', tz='Asia/Kolkata'),
+                         1: Timestamp('2019-06-21 14:30:00+0530', tz='Asia/Kolkata'),
+                         2: Timestamp('2019-02-26 17:30:00+0530', tz='Asia/Kolkata'),
+                         3: Timestamp('2019-06-12 18:15:00+0530', tz='Asia/Kolkata'),
+                         4: Timestamp('2019-01-11 11:00:00+0530', tz='Asia/Kolkata')},
+            'id': {0: 'sdacdcasd23vasdvasdvas',
+                   1: 'sdacdca223sdvasdvasdvas',
+                   2: 'sdacdcas233dvasdvasdvas',
+                   3: 'sdac2dca2sdvasdvasdvas',
+                   4: 'sda3cdcasdvasdvasdvas'},
+            'start_time': {0: Timestamp('2019-03-15 12:00:00+0530', tz='Asia/Kolkata'),
+                           1: Timestamp('2019-06-21 14:00:00+0530', tz='Asia/Kolkata'),
+                           2: Timestamp('2019-02-26 16:30:00+0530', tz='Asia/Kolkata'),
+                           3: Timestamp('2019-06-12 17:00:00+0530', tz='Asia/Kolkata'),
+                           4: Timestamp('2019-01-11 10:00:00+0530', tz='Asia/Kolkata')},
+            'time_spent': {0: 3600, 1: 1800, 2: 3600, 3: 4500, 4: 3600},
+            'title': {0: 'CV review-Senior Full Stack Developer',
+                      1: 'Grower onboarding | show and tell ',
+                      2: 'Assignment Review',
+                      3: 'Interview (Lead Mobile Developer)',
+                      4: 'Standup catchcup'}}
+
+    def setUp(self):
+        self.user = User.objects.create(username='admin',
+                                        email='admin@admin.com')
+
+        self.calendar = Calendar.objects.create(user=self.user,
+                                                cal_id=self.user.email,
+                                                title=self.user.email,
+                                                timezone='Asia/Kolkata',
+                                                events_sync_token="events_sync_token")
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test__search_meetings(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca._search_meetings(['Standup']).to_dict()
+        expected_output = {'end_time': {4: Timestamp('2019-01-11 10:00:00+0530', tz='Asia/Kolkata')},
+                           'id': {4: 'sda3cdcasdvasdvasdvas'},
+                           'start_time': {4: Timestamp('2019-01-11 10:00:00+0530', tz='Asia/Kolkata')},
+                           'time_spent': {4: 3600},
+                           'title': {4: 'Standup catchcup'}}
+        self.assertDictEqual(actual_output, expected_output)
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test_time_spent_on(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca.time_spent_on(['Standup']).to_dict()
+        expected_output = 3600
+        self.assertEqual(actual_output, expected_output)
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test_total_time_spent_by_month(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca.total_time_spent_by_month()
+        expected_output = {'February': 3600, 'January': 3600, 'June': 6300, 'March': 3600}
+        self.assertEqual(actual_output, expected_output)
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test_total_time_spent_by_week(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca.total_time_spent_by_week()
+        expected_output =  {'01': 3600, '08': 3600, '10': 3600, '23': 4500, '24': 1800}
+        self.assertEqual(actual_output, expected_output)
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test_avg_time_spent_by_week(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca.avg_time_spent_by_week()
+        expected_output = {'01': 3600, '08': 3600, '10': 3600, '23': 4500, '24': 1800}
+        self.assertEqual(actual_output, expected_output)
+
+    @mock.patch.object(CalendarAnalytics, "_events_df")
+    def test_avg_meetings_by_week(self, _events_df):
+        _events_df.return_value = pd.DataFrame(self.DATA)
+        ca = CalendarAnalytics(self.user)
+        actual_output = ca.avg_meetings_by_week()
+        expected_output = {'01': 1, '08': 1, '10': 1, '23': 1, '24': 1}
+        self.assertEqual(actual_output, expected_output)
